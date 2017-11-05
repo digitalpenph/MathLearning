7ac49fd23a4cc9f938f2706649664ee42ae1e9b1
Tristan Jake Alcantara <alcantaratristanjake@gmail.com>
2017-11-05 19:52:32 +0800

    Added async storage.

--- app/StartScreen.js
+++ app/StartScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableHighlight, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View, StatusBar, AsyncStorage, Alert } from 'react-native';
+
 
 export default class StartScreen extends Component {
   static navigationOptions = {
@@ -7,33 +8,36 @@ export default class StartScreen extends Component {
     headerStyle: { backgroundColor: '#006A40' },
     headerTitleStyle: { color: '#FFFFFF' },
   };
+  navigateLesson() {
+    let lessonid = '0';
+    AsyncStorage.setItem('lessonid', lessonid);
+    this.props.navigation.navigate('Lesson');
+  }
   render() {
-    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-	<Lessons />
-        <TouchableHighlight onPress={() => navigate('Lesson')} underlayColor='#FFFFFF'>
+        <TouchableHighlight onPress={this.navigateLesson.bind(this)} underlayColor='#FFFFFF'>
           <View style={styles.button}>
             <Text style={styles.buttonText}>LESSON 1</Text>
           </View>
         </TouchableHighlight>
-        <TouchableHighlight disabled={true} onPress={() => navigate('Lesson')} underlayColor='#FFFFFF'>
+        <TouchableHighlight disabled={true} onPress={this.navigateLesson} underlayColor='#FFFFFF'>
           <View style={styles.button_disabled}>
             <Text style={styles.buttonText}>LESSON 2</Text>
           </View>
         </TouchableHighlight>
-        <TouchableHighlight disabled={true} onPress={() => navigate('Lesson')} underlayColor='#FFFFFF'>
+        <TouchableHighlight disabled={true} onPress={this.navigateLesson} underlayColor='#FFFFFF'>
           <View style={styles.button_disabled}>
             <Text style={styles.buttonText}>LESSON 3</Text>
           </View>
         </TouchableHighlight>
-        <TouchableHighlight disabled={true} onPress={() => navigate('Lesson')} underlayColor='#FFFFFF'>
+        <TouchableHighlight disabled={true} onPress={this.navigateLesson} underlayColor='#FFFFFF'>
           <View style={styles.button_disabled}>
             <Text style={styles.buttonText}>LESSON 4</Text>
           </View>
         </TouchableHighlight>
-        <TouchableHighlight disabled={true} onPress={() => navigate('Lesson')} underlayColor='#FFFFFF'>
+        <TouchableHighlight disabled={true} onPress={this.navigateLesson} underlayColor='#FFFFFF'>
           <View style={styles.button_disabled}>
             <Text style={styles.buttonText}>LESSON 5</Text>
           </View>
