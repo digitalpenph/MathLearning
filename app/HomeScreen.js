9fb2d61e76f5ef4a6090574ad96cd564e7d155d1
taalcantara <taalcantara@dilc.upd.edu.ph>
2017-11-21 17:26:58 +0800

    Added animations and background sound

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, TouchableHighlight, View, StatusBar } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; // 6.1.0
+import * as Animatable from 'react-native-animatable'; // 1.2.4
+import { Audio } from 'expo';
 
 export default class HomeScreen extends Component {
   static navigationOptions = {
@@ -8,35 +10,41 @@ export default class HomeScreen extends Component {
     headerStyle: { backgroundColor: '#006A40' },
     headerTitleStyle: { color: '#FFFFFF' },
   };
+  async componentDidMount() {
+    const sound = new Audio.Sound();
+    await sound.loadAsync(require('../assets/sounds/sound.mp3'));
+    await sound.setIsLoopingAsync(true);
+    await sound.playAsync();
+  }
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-	<TouchableHighlight onPress={() => navigate('Review')} underlayColor='#FFFFFF'>
-          <View style={styles.button}>
+        <TouchableHighlight onPress={() => navigate('Review')} underlayColor='#FFFFFF'>
+          <Animatable.View animation="rubberBand" iterationCount="infinite" easing="ease-out" delay={100} duration={3000} style={styles.button}>
             <Ionicons style={styles.buttonText}  name="md-paper" size={25} color="white"> REVIEW</Ionicons>
-          </View>
+          </Animatable.View>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => navigate('Quiz')} underlayColor='#FFFFFF'>
-          <View style={styles.button}>
+          <Animatable.View animation="shake" iterationCount="infinite" easing="ease-out" delay={200} duration={3000} style={styles.button}>
             <Ionicons style={styles.buttonText}  name="md-create" size={25} color="white"> QUIZ</Ionicons>
-          </View>
+          </Animatable.View>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => navigate('Formula')} underlayColor='#FFFFFF'>
-          <View style={styles.button}>
+          <Animatable.View animation="swing" iterationCount="infinite" easing="ease-out" delay={300} duration={3000} style={styles.button}>
             <Ionicons style={styles.buttonText}  name="md-easel" size={25} color="white"> FORMULAS</Ionicons>
-          </View>
+          </Animatable.View>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => navigate('Dictionary')} underlayColor='#FFFFFF'>
-          <View style={styles.button}>
+          <Animatable.View animation="tada" iterationCount="infinite" easing="ease-out" delay={400} duration={3000} style={styles.button}>
             <Ionicons style={styles.buttonText}  name="md-book" size={25} color="white"> DICTIONARIES</Ionicons>
-          </View>
+          </Animatable.View>
         </TouchableHighlight>
         <TouchableHighlight onPress={() => navigate('About')} underlayColor='#FFFFFF'>
-          <View style={styles.button}>
+          <Animatable.View animation="jello" iterationCount="infinite" easing="ease-out" delay={500} duration={3000} style={styles.button}>
             <Ionicons style={styles.buttonText}  name="md-contacts" size={25} color="white"> ABOUT</Ionicons>
-          </View>
+          </Animatable.View>
         </TouchableHighlight>
       </View>
     );
@@ -52,7 +60,7 @@ const styles = StyleSheet.create({
   },
   button: {
     borderRadius: 50,
-    marginBottom: 30,
+    margin: 15,
     width: 250,
     alignItems: 'center',
     backgroundColor: '#006A40'
